730841517b4481e2a216d0fbc92ffcb499d5af75
IvashkinMikhail <ivashkinmd@inbox.ru>
2019-02-02 17:53:36 +0300

    1.3 JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,15 @@
 package ru.job4j.calculate;
-
+/**
+*Class с точкой входа в программу
+*@author IvashkinMD
+*@comment added 02/02/2019
+*/
 public class Calculate {
-	
+	/**
+	*Метод точки входа в программу
+	*@comment added 02/02/2019
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
 	}
-}
+} 
