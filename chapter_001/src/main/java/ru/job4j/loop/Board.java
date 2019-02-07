ebba7d30cf17ac38ac4535d1d950cabf129a4953
IvashkinMikhail <ivashkinmd@inbox.ru>
2019-02-07 23:24:23 +0300

    5.3 ш доска

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+public class Board {
+
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 1; i <= width; i++) {
+            for (int k = 1; k <= height; k++) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if ((k + i) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
