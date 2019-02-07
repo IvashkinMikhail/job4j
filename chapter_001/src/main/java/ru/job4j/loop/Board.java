17ece0bef3ce6445e8a544b42a8bff97127128d4
IvashkinMikhail <ivashkinmd@inbox.ru>
2019-02-07 23:29:41 +0300

    5.3 ш доска

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -5,8 +5,8 @@ public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        for (int i = 1; i <= width; i++) {
-            for (int k = 1; k <= height; k++) {
+        for (int i = 1; i <= height; i++) {
+            for (int k = 1; k <= width; k++) {
                 // условие проверки, что писать пробел или X
                 // Выше в задании мы определили закономерность, когда нужно проставлять X
                 if ((k + i) % 2 == 0) {
