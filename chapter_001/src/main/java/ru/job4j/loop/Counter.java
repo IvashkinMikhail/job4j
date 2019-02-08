88982f9be21f655c325e8fcd521f3966b10895e2
IvashkinMikhail <ivashkinmd@inbox.ru>
2019-02-08 20:33:09 +0300

    5.1 проверка валидацией

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -5,8 +5,9 @@ public class Counter {
 
     public int add(int start, int finish) {
         int p = 0;
-        for (int i = start; i <= finish; i++)
+        for (int i = start; i <= finish; i++) {
             p += i % 2 == 0 ? i : 0;
+        }
         return p;
     }
 
