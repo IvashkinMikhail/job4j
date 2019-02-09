c4a6405e0f2b8937e57c730a4bc67f09ea0a5a59
IvashkinMikhail <ivashkinmd@inbox.ru>
2019-02-10 00:25:35 +0300

    6.5 пузырек

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class BubbleSort {
+
+    public int[] sort(int[] array) {
+        int p;
+        for (int j = 0; j <= array.length; j++) {
+            for (int i = 0; i <= array.length - 2-j; i++) {
+                if (array[i] > array[i + 1]) {
+                    p = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = p;
+                }
+            }
+        }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BubbleSortTest {
+    @Test
+    public void whenSortArrayWithTenElementsThenSortedArray() {
+        //{1, 5, 4, 2, 3, 1, 7, 8, 0, 5}
+        BubbleSort bubble = new BubbleSort();
+        int[] array = new int[]{1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
+        int[] result = bubble.sort(array);
+        int[] expect = {0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
+        assertThat(result, is(expect));
+    }
+}
+
