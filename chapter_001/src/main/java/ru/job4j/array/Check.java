d62a0e78dbb20117764940217b5acc8194f848f8
IvashkinMikhail <ivashkinmd@inbox.ru>
2019-02-10 23:43:37 +0300

    6.3 улучшение кода к заданию

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,14 +2,11 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
         for (int i = 0; i <= data.length - 1; i++) {
             if (data[0] != data[i]) {
                 return false;
-            } else {
-                result = true;
             }
         }
-        return result;
+        return true;
     }
 }
